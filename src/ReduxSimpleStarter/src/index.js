431cb9eb56ec64ca6358dd797d3db81330395c2f
Philippe Le Van <philippe.levan@kitpages.fr>
2018-02-23 13:26:23 +0100

    one simple component front scratch

--- src/ReduxSimpleStarter/src/index.js
+++ src/ReduxSimpleStarter/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component. This component should produce some html
+/*
+const App = function() {
+    return <div>Hi !</div>
+}
+*/
+const App = () => {
+    return <div>Hi !!</div>
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// take this components generated htmland put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
