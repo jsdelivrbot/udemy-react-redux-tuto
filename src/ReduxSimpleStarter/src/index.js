1d7bed14a49d7760a64552600c75d2c3fa9d2370
Philippe Le Van <philippe.levan@kitpages.fr>
2018-02-23 17:30:30 +0100

    video display, and click on list

--- src/ReduxSimpleStarter/src/index.js
+++ src/ReduxSimpleStarter/src/index.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCXH3HFy7bufyh6eFL2r7qnXJ3zbdtuQd8';
 
 // create a new component. This component should produce some html
+class App extends Component
+{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videoList: [],
+            selectedVideo: null
+        };
+
 /*
-const App = function() {
-    return <div>Hi !</div>
-}
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videoList) => {
+            this.setState({ videoList: videoList});
+        });
 */
-const App = () => {
-    return (
-        <div>
-            Hi !! <SearchBar />
-        </div>
-    );
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videoList) => {
+            this.setState({
+                videoList: videoList,
+                selectedVideo: videoList[0]
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    videoList={this.state.videoList}
+                    onVideoSelect={ selectedVideo =>  this.setState({selectedVideo}) }
+                />
+            </div>
+        );
+    }
 }
 
 // take this components generated htmland put it on the page
