7b1894be9350e6332e5ff096611837738871e1cb
Philippe Le Van <philippe.levan@kitpages.fr>
2018-02-23 15:40:13 +0100

    controlled input

--- src/ReduxSimpleStarter/src/components/search_bar.js
+++ src/ReduxSimpleStarter/src/components/search_bar.js
@@ -3,13 +3,14 @@ import React, {Component} from 'react'
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {term: ''};
+        this.state = {term: 'yo man'};
     }
 
     render() {
         return (
             <div>
                 <input
+                    value={this.state.term}
                     onChange={(event) => this.setState({ term: event.target.value })}
                     onFocus={this.onFocusChange}
                 />
