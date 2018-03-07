77a389c27d4b9f45a2228be47642dd6bde74956b
Philippe Le Van <philippe.levan@kitpages.fr>
2018-03-07 16:53:12 +0100

    validation of the form

--- src/ReduxRouterForm/src/components/posts_new.js
+++ src/ReduxRouterForm/src/components/posts_new.js
@@ -12,6 +12,7 @@ class PostsNew extends Component {
                     type="text"
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         );
     }
@@ -39,8 +40,26 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    console.log(values);
+    const errors = {};
+    // validate the inputs from 'values'
+    if (!values.title || values.title.length < 2) {
+        errors.title = "Enter a title with more than 2 chars !";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter a Category !";
+    }
+    if (!values.content) {
+        errors.content = "Enter a content !";
+    }
 
+    // if errors is empty, the form is fine to submit
+    // if errors has any property, redux form assumes form is invalid
+    return errors;
+}
 
 export default reduxForm({
+    validate: validate,
     form: 'PostsNewForm'
 })(PostsNew);
