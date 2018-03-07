28d451c89794598f45afaa9c0de0a5612547b940
Philippe Le Van <philippe.levan@kitpages.fr>
2018-03-07 16:34:50 +0100

    add form Fields in new post

--- src/ReduxRouterForm/src/components/posts_new.js
+++ src/ReduxRouterForm/src/components/posts_new.js
@@ -1,15 +1,49 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div className="form-group">
+                <label>Title</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
+    renderTagsField(field) {
+        return (
+            <div className="form-group">
+                <label>Tags</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
-                <form>
-                    <input />
-                </form>
-            </div>
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+                <Field
+                    name="tags"
+                    component={this.renderTagsField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
