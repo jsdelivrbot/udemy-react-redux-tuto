4d2ac6d6d93b33f83e07fc75fe298e062fc0a40f
Philippe Le Van <philippe.levan@kitpages.fr>
2018-03-07 09:54:42 +0100

    basic router with tests elements

--- src/ReduxRouterForm/src/index.js
+++ src/ReduxRouterForm/src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
+
+class Hello extends React.Component {
+    render() {
+        return <div>Hello! <a href="/goodbye">exit</a></div>
+    }
+}
+class Goodbye extends React.Component {
+    render() {
+        return <div>Goodbye!<a href="/hello">back</a></div>
+    }
+}
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+      <BrowserRouter>
+          <div>
+              Header
+              <Route path="/hello" component={Hello}/>
+              <Route path="/goodbye" component={Goodbye}/>
+          </div>
+      </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
