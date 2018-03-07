7ae2ecc55a9c848866d4f611f140d1297e6e83aa
Philippe Le Van <philippe.levan@kitpages.fr>
2018-03-07 10:05:45 +0100

    cleanup for router test and removing of app component

--- src/ReduxRouterForm/src/index.js
+++ src/ReduxRouterForm/src/index.js
@@ -1,22 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
-
-class Hello extends React.Component {
-    render() {
-        return <div>Hello! <a href="/goodbye">exit</a></div>
-    }
-}
-class Goodbye extends React.Component {
-    render() {
-        return <div>Goodbye!<a href="/hello">back</a></div>
-    }
-}
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -24,9 +12,6 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
           <div>
-              Header
-              <Route path="/hello" component={Hello}/>
-              <Route path="/goodbye" component={Goodbye}/>
           </div>
       </BrowserRouter>
   </Provider>
